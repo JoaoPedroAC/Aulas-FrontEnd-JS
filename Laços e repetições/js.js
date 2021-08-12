991c5854ef3c8f0ec02879022063b6d11ad723fc
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-08-11 21:13:09 -0300

    Update js.js

--- Laços e repetições/js.js
+++ Laços e repetições/js.js
@@ -22,17 +22,34 @@ window.onload = function() {
     } while (nome == 'gustavo') {
         console.log('Mentira');
     }*/
-    function laco2(n) {
+
+    /*function laco2(n) {
         for (i = 0; i < 10; i++) {
             n++;
-            /* if (n == 3) {
-                 // continue; //vai pular o numero 3, mas vai continuar o loop
-             };
-             if (n == 9) {
-                 // break; //vai parar antes do 9, mas dependendo da posição do .log pode parar no nove tbm
-             }*/
+            if (n == 3) {
+                // continue; //vai pular o numero 3, mas vai continuar o loop
+            };
+            if (n == 9) {
+                // break; //vai parar antes do 9, mas dependendo da posição do .log pode parar no nove tbm
+            }
             console.log(n);
         };
     };
-    laco2(0);
+    laco2(0);*/
+
+    function Nome() {
+        let nome = 'João'
+            // é um laço tbm, onde switch onde fará d alvo a variavel
+        switch (nome) {
+            // aki eh onde vai checar se eh ou nao igual; pena q eh bem limitado essa funçao
+            case 'Gustavo':
+                // o break é pra parar se nao ele vai ficar repetindo o laço direto
+                break;
+            case 'João':
+                // aki veremos o resultaddo
+                alert('ola ' + nome);
+                break;
+        }
+    }
+    Nome()
 };
