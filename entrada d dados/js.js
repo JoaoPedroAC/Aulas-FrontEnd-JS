c97d3cbc447406445113d4fef9afce881cb9304c
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-08-24 19:45:21 -0300

    add

---
+++ entrada d dados/js.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var nome = prompt('Qual é o seu nome? ');
+    var idade = prompt('Qual é sua idade? ');
+    var cidade = prompt('Qual é a sua cidade? ');
+
+    console.log('seu nome é: ' + nome);
+    console.log('sua idade é: ' + idade);
+    console.log('sua cidade é: ' + cidade);
+}
