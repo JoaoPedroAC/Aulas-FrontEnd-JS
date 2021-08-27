321f4f2df0c5a896301fcbd55f27944a0f685585
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-08-27 07:58:02 -0300

    add aulas novas

---
+++ Objetos/js.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var pessoas = [{
+            'nome': 'João',
+            'idade': 21,
+            'cargo': 'desenvolvedor'
+        },
+        {
+            'nome': 'Arthur',
+            'idade': 23,
+            'cargo': 'gay'
+
+        }
+    ];
+    console.log(pessoas[1].cargo)
+};
